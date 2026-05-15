3c1f88deb2f67d09509ba5d6e75e91bfea42927e
m9234601-lang <m9234601@gmail.com>
2026-05-15 10:50:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-pro')
 else:
     st.sidebar.error("❌ GOOGLE_API_KEY를 설정해주세요.")
 
